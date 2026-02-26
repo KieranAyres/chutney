64d1a566d480639acd66dd00e8c9ade04d34f577
Kieran Ayres <Kieran.Ayres@dvla.gov.uk>
2026-02-26 18:56:59 +0000

    feat: linting issues

--- lib/chutney/linter/avoid_single_string_tag_names.rb
+++ lib/chutney/linter/avoid_single_string_tag_names.rb
@@ -1,9 +1,14 @@
+# frozen_string_literal: true
+
 module Chutney
+  # service class to lint for single character tags, such as @t
+
   class AvoidSingleStringTagNames < Linter
     def lint
       scenarios do |feature, scenario|
         tags = tags_for(feature) + tags_for(scenario)
         next unless tags.any? { |tag| tag.length == 1 }
+
         add_issue(
           I18n.t('linters.avoid_single_string_tag_names'),
           feature
@@ -12,5 +17,3 @@ module Chutney
     end
   end
 end
-
-# 1st approach. We filter out the tags to only look for singular tags
